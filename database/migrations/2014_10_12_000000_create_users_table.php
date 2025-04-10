c44caac2c4ac36dea8b267941a48d4a48d68ac9f
Alejandro Parrilla <43898056+ajpcve@users.noreply.github.com>
2025-04-10 13:18:02 -0400

    add softDeletes in users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -28,6 +28,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
